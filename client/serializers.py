8b5b81a397b3e2be2bb63fc7f55e88cfdecf410c
Willian Alves <wiilliian.alves@gmail.com>
2018-03-26 19:09:00 -0400

    adicionando serializers de cliente

---
+++ client/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import Client
+
+class ClientSerializer(ModelSerializer):
+    class Meta:
+        model = Client
+        fields = [
+            'id', 'name' 'address'
+        ]
