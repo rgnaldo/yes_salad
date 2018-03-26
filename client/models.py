ebed84011d14f2d00aacf34132fad11b028c110e
Willian Alves <wiilliian.alves@gmail.com>
2018-03-26 19:08:36 -0400

    adicionando modelo de cliente

--- client/models.py
+++ client/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(verbose_name='name', max_length=100)
+    address = models.CharField(verbose_name='address', max_length=100)
+
+    def __str__(self):
+        return "{0}".format(self.name)
