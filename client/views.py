c31277aaee95d02ab3431fee9b42dba8cf00088f
Willian Alves <wiilliian.alves@gmail.com>
2018-03-27 10:41:53 -0400

    ajustes no serializers

--- client/views.py
+++ client/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 
-from client import serializers
+from client.serializers import ClientSerializer
 from client.models import Client
 
 
 class ClientViewSet(ModelViewSet):
-    serializer_class = serializers.ClientSerializer
     queryset = Client.objects.all()
+    serializer_class = ClientSerializer
