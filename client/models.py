114193d232601593986c122fcfa9e827e1531e4d
Willian Alves <wiilliian.alves@gmail.com>
2018-03-27 16:41:28 -0400

    alteração no model de cliente

--- client/models.py
+++ client/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class Client(models.Model):
-    name = models.CharField(verbose_name='name', max_length=100)
-    address = models.CharField(verbose_name='address', max_length=100)
-    contact = models.CharField(verbose_name='contact', max_length=20)
+    name = models.CharField(verbose_name='Nome', max_length=100)
+    address = models.CharField(verbose_name='Endereço', max_length=100)
+    contact = models.CharField(verbose_name='Contato', max_length=20)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return "{0}".format(self.name)
