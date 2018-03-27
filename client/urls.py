90b2665e4df7caaa5dd8a99957cd169836f7dc91
Willian Alves <wiilliian.alves@gmail.com>
2018-03-27 16:03:57 -0400

    melhorando urls

--- client/urls.py
+++ client/urls.py
@@ -2,7 +2,9 @@ from rest_framework.routers import DefaultRouter
 
 from client.views import ClientViewSet
 
+urlpatterns = []
 
 router = DefaultRouter()
-
 router.register('', ClientViewSet)
+
+urlpatterns = urlpatterns + router.urls
--- yes_salad/urls.py
+++ yes_salad/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from client.urls import router
-
 
 urlpatterns = [
-    path('client/', include(router.urls)),
+    path('client/', include('client.urls')),
     path('admin/', admin.site.urls),
 ]
