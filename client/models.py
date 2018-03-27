9ee7df47f520901770dc2ffb51883f9f6cdc7994
Willian Alves <wiilliian.alves@gmail.com>
2018-03-27 16:28:52 -0400

    adicionando atributo contact no model de cliente

--- client/models.py
+++ client/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField(verbose_name='name', max_length=100)
     address = models.CharField(verbose_name='address', max_length=100)
+    contact = models.CharField(verbose_name='contact', max_length=20)
 
     def __str__(self):
         return "{0}".format(self.name)
