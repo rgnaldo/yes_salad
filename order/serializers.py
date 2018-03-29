2ef6c1f99a5e411c89692d29a2eff55a57b6681d
Naldo <rgnaldo1@gmail.com>
2018-03-29 07:33:59 -0400

    corrigindo os conflitos do order/serialires

--- order/serializers.py
+++ order/serializers.py
@@ -12,9 +12,5 @@ class OrderSerializer(ModelSerializer):
     class Meta:
         model = Order
         fields = [
-<<<<<<< 0ddf37c4a44782b2c24e2664bd4d6ef636212e4b
             'client', 'menu',  'creation_date', 'quantity', 'total_value',
-=======
-            'client', 'menu',
->>>>>>> erro serializers
-            ]
+        ]
