5ca1f8cc7580b50888522ce07a59d03eae8d39c1
Naldo <rgnaldo1@gmail.com>
2018-03-28 21:01:02 -0400

    erro models

--- buy/models.py
+++ buy/models.py
@@ -7,5 +7,3 @@ class Buy(models.Model):
     order = ForeignKey(Order, on_delete=PROTECT, )
     creation_date = DateTimeField(auto_now_add=True)
     quantidade = models.IntegerField(verbose_name='Quantidade')
-    valor_unitario = models.FloatField(verbose_name='Valor Unitario')
-    valor_total = models.FloatField(verbose_name='Valor Total')
